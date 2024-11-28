9515d21a6b8f86bbaefa7288cefb09bd8a856fb9
Kacper Jóźwik <kacperjozwik12@gmail.com>
2024-11-28 16:58:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col h-screen items-center justify-center'>
+      <h1 className='text-black'>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
